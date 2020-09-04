88f42463de8f3d8f6808a81e611a3345f19b3a1a
Janet Karpenske <janetkarpenske@gmail.com>
2020-09-04 15:17:29 -0700

    Add styling to make columns appear/disappear, fix indentations, add more ui

--- _tests_/solar.test.js
+++ _tests_/solar.test.js
@@ -7,36 +7,28 @@ describe('Galactic', () => {
   beforeEach(() => {
     galactic = new Galactic(24, "North America");
   });
-
   test('should create an instance of Galactic', () => {
     expect(galactic).toEqual({userAge: 24, continent: "North America"});
   });
-
   test('should return a life expectancy number', () => {
     expect(galactic.lifeExpect()).toEqual(80);
   });
-
   test('should return the users age based on mercury years', () => {
     expect(galactic.mercury()).toEqual(100);
   });
-
   test('should return the users age based on Venus years', () => {
     expect(galactic.venus()).toEqual(39);
   });
-
   test('should return the users age based on Mars years', () => {
     expect(galactic.mars()).toEqual(13);
   });
-
   test('should return the users age based on Mars years', () => {
     expect(galactic.jupiter()).toEqual(2);
   });
-
   test('adds mercury years to constructor', () => {
     galactic.lifeExpect();
     expect(galactic.lifeExpectancy).toEqual(80);
   });
-
   test('makes life expectancy on other planets a property of constructor', () => {
     galactic.lifeExpect();
     galactic.mercury();
--- src/js/main.js
+++ src/js/main.js
@@ -3,14 +3,17 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/styles.css';
 import { Galactic } from './solar.js';
-
 function displayStats(merAge, venAge, marAge, jupAge, galactic) {
   $("#merAge").text(merAge);
   $("#venAge").text(venAge);
   $("#marAge").text(marAge);
   $("#jupAge").text(jupAge);
+  $("#merEx").text(galactic.mercuryExpect);
+  $("#venEx").text(galactic.venusExpect);
+  $("#marsEx").text(galactic.marsExpect);
+  $("#jupEx").text(galactic.jupiterExpect);
+  $("#results").show();
 }
-
 $(document).ready(function() { 
   $("#form").submit(function(event) {
     event.preventDefault();
