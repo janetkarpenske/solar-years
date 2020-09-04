d39c89fb253e43202ae6edcfb0e7bd6aa21d025f
Janet Karpenske <janetkarpenske@gmail.com>
2020-09-04 11:04:28 -0700

    Pass test to save users life expectancy as property of constructor

--- src/js/solar.js
+++ src/js/solar.js
@@ -7,22 +7,27 @@ export class Galactic {
 
   lifeExpect() {
     if (this.continent === "North America") {
-      
+      this.lifeExpectancy = 80;
       return 80;
     }
     if (this.continent === "South America") {
+      this.lifeExpectancy = 70;
       return 76;
     }
     if (this.continent === "Europe") {
+      this.lifeExpectancy = 81;
       return 81;
     }
     if (this.continent === "Africa") {
+      this.lifeExpectancy = 65;
       return 65;
     }
     if (this.continent === "Asia") {
+      this.lifeExpectancy = 78;
       return 78;
     }
     if (this.continent === "Australia") {
+      this.lifeExpectancy = 82;
       return 82;
     } }
 
