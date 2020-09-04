64296b2e73023c2c1878922a0436979eb17a3b13
Janet Karpenske <janetkarpenske@gmail.com>
2020-09-04 13:46:25 -0700

    Add basic form, some css styling, and some ui to main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,16 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './../css/styles.css';
-// import { Galactic } from './solar.js';
+import '../css/styles.css';
+//import { Galactic } from './solar.js';
 
 $(document).ready(function() { 
   $("#form").submit(function(event) {
     event.preventDefault();
-    
+    const userAge = parseInt($("#age").val());
+    const continent = $("#continent").val();
+    //let galactic = new Galactic(userAge, continent);
+
   })
   // let galactic = new Galactic(24, "North America");
   // console.log(galactic);
